b95af7f0650233e3b76bd812afad9310d5adff8a
lyubomir <lyubomir.d.dimov@gmail.com>
2017-09-01 11:11:28 +0200

    Init

---
+++ AleConsoleApp/AleConsoleApp/AleConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
