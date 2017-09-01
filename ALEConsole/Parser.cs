67d86959bb206c67c35706ac00570c2c31ec5eb9
lyubomir <lyubomir.d.dimov@gmail.com>
2017-09-01 11:48:44 +0200

    parser init

--- ALEConsole/Parser.cs
+++ ALEConsole/Parser.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ALEConsole
 {
     public static class Parser
     {
+        public static string RemoveWhiteSpaces(this string input)
+        {
+            string pattern = "\\s+";
+            string replacement = "";
+            Regex rgx = new Regex(pattern);
+            return rgx.Replace(input, replacement);
+        }
+
+        public static void ParseToTree(this string input)
+        {
+            
+        }
     }
 }
