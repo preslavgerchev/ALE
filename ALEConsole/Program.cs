117aa3717a5edbf15d30fe85a61e01d61814fb46
Preslav Gerchev <preslavgerchevmail@gmail.com>
2017-09-01 11:35:56 +0200

    .NET core to .NET

---
+++ ALEConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALEConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
