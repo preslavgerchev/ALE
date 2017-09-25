c2d4ab876c5ae51cf46f288ad8513175e8c517cf
Preslav Gerchev <preslavgerchevmail@gmail.com>
2017-09-25 22:28:21 +0200

    Fixed some documentation.

--- AutomataLogicEngineering/Symbols/ParenthesisSide.cs
+++ AutomataLogicEngineering/Symbols/ParenthesisSide.cs
@@ -5,8 +5,14 @@
     /// </summary>
     public enum ParenthesisSide
     {
+        /// <summary>
+        /// Represents an opening parenthesis.
+        /// </summary>
         Opening = 0,
 
+        /// <summary>
+        /// Represents a closing parenthesis.
+        /// </summary>
         Closing = 1
     }
 }
