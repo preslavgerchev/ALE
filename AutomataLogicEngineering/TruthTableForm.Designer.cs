c83908c561732c6710728a1df9567f098862d95f
Preslav Gerchev <preslavgerchevmail@gmail.com>
2017-09-27 00:01:02 +0200

    Small UI changes, disabled both truth table views and added hex label.

--- AutomataLogicEngineering/TruthTableForm.Designer.cs
+++ AutomataLogicEngineering/TruthTableForm.Designer.cs
@@ -33,6 +33,7 @@
             this.truthTableLbl = new System.Windows.Forms.Label();
             this.simplifiedTruthTableLbl = new System.Windows.Forms.Label();
             this.hexadecimalLbl = new System.Windows.Forms.Label();
+            this.hexadecimalValueLbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.truthTableView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.simplifiedTruthTableView)).BeginInit();
             this.SuspendLayout();
@@ -40,6 +41,7 @@
             // truthTableView
             // 
             this.truthTableView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.truthTableView.Enabled = false;
             this.truthTableView.Location = new System.Drawing.Point(11, 29);
             this.truthTableView.Margin = new System.Windows.Forms.Padding(2);
             this.truthTableView.Name = "truthTableView";
@@ -50,6 +52,7 @@
             // simplifiedTruthTableView
             // 
             this.simplifiedTruthTableView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.simplifiedTruthTableView.Enabled = false;
             this.simplifiedTruthTableView.Location = new System.Drawing.Point(11, 332);
             this.simplifiedTruthTableView.Margin = new System.Windows.Forms.Padding(2);
             this.simplifiedTruthTableView.Name = "simplifiedTruthTableView";
@@ -83,11 +86,21 @@
             this.hexadecimalLbl.Size = new System.Drawing.Size(0, 13);
             this.hexadecimalLbl.TabIndex = 8;
             // 
+            // hexadecimalValueLbl
+            // 
+            this.hexadecimalValueLbl.AutoSize = true;
+            this.hexadecimalValueLbl.Location = new System.Drawing.Point(348, 9);
+            this.hexadecimalValueLbl.Name = "hexadecimalValueLbl";
+            this.hexadecimalValueLbl.Size = new System.Drawing.Size(58, 13);
+            this.hexadecimalValueLbl.TabIndex = 9;
+            this.hexadecimalValueLbl.Text = "Hex value:";
+            // 
             // TruthTableForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(938, 638);
+            this.Controls.Add(this.hexadecimalValueLbl);
             this.Controls.Add(this.hexadecimalLbl);
             this.Controls.Add(this.simplifiedTruthTableLbl);
             this.Controls.Add(this.truthTableLbl);
@@ -110,5 +123,6 @@
         private System.Windows.Forms.Label truthTableLbl;
         private System.Windows.Forms.Label simplifiedTruthTableLbl;
         private System.Windows.Forms.Label hexadecimalLbl;
+        private System.Windows.Forms.Label hexadecimalValueLbl;
     }
 }
